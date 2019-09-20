36821b0388781f559f8b963db16ed515b22ae015
miko_da_freako <53712054+mikodafreako@users.noreply.github.com>
2019-09-19 21:41:28 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,248 +1,132 @@
-//Our main js code called by Jquery on doc ready
-    $(document).ready(function () {
-        //game variables    
-        var message,                //screen message to display
-        hits = 10,                  //hit points for the player
-        lightLevel = 100,           //current light level
-        currentRoom = 0,            //initial room  
-        exitRoom = 31,              //final room of the dungeon
-        IsGameOver = false;         //Maintain the state of the game
-        IsOgreAlive = true,         //Stores the state of the Ogre - Alive/Dead
-        IsDragonAlive = true;       //this is the gameover state
-        //All the commands we use in the game
-        var gameWords = new Array("HELP", "Find/earch", "N-orth", "S-outh", "W-est", "E-east','A-About");
-        //All the rooms in the game
-        var rooms = new Array("Dungeon Entrance", "Corridor of uncertainty", 'Ancient old cavern', "Great Cavern", "Underground River", "Stream", 'Dungeon Stream', "Dungeon Pool",
-                              "Large Cavern", "Rough Tunnell", "Long Tunnell", "Dark Room", "Dark Room", "Cold Room", "Old Tunnel", "Cold Room",
-                              "Old Cavern", "Short Corridor", "Short Corridor", "Grey Room", "Green Room", "Old Prison Cell", "Underground River",
-                              "Large Cavern", "Rough Tunnell", "Long Tunnell", "Dark Room", "Dark Room", "Cold Room", "Old Tunnel", "Dragons Room");
-        //Each exit relates to the index ie. Exits[0] SE which means rooms[0] the long path has two exits on the  South and East. If we look
-        //down to the //Movement Code section you can see how we work out which rooms are connected to which
-        var exits = new Array("E", "SWE", "WE", "SWE", "WE", "WE", "SWE", "WS",
-                              "NSE", "SE", "WE", "NW", "SE", "W", "SNE", "NSW",
-                              "NS", "NS", "SE", "WE", "NWE", "SWE", "WS", "N",
-                              "N", "NWE", "NWE", "WE", "WE", "NW", "NE", "W");
-        //All out game objects
-        var GameObjects = new Array('', "Painting", "Knife", "Wand of Firebolts", "Goblet", "Wand of Wind", "Coins", "Helmet", "Candle", "Torch", "Iron Shield", "Armour", "Oil", "AXE", "ROPE", "BOAT", "AEROSOL", "CANDLE", "KEY");
-        //Inventory array Contains all the things you can carry
-        var inventory = new Array();
-        inventory[0] = 2; //lets start our player off with a knife
-        //location of game objects - these objects relate to a array index - so Object[1] the Painting is located
-        //in rooms[2] the small garden - 999 indicates out of play 
-        var objectLocations = [999, 1, 999, 3, 4, 5, 6, 7, 8, 10, 11, 15, 14, 12, 18, 19, 16, 17, 9]
-        //This function detects if the browser if a mobile - you'll see when we call this we apply the 
-        function isMobile() {
-            return navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i)
-                    || navigator.userAgent.match(/Opera Mini/i) || navigator.userAgent.match(/IEMobile/i);
-        }
-        //The next line checks for a mobile browser and if it find st it will hide the buttons or hide the text box
-        if (isMobile()) {
-            //hide the text box - we dont need that for a mobile browser as its hard to use mobile keyboard for lots of commands
-            $("#Keyboard").hide();
-        } else {
-            //hide the buttons as we don't want that for the normal web experience
-            $('#controllers').hide();
-            //jquery command to force the textbox to take focus  
-            $("#userInput").focus();
-        }
-        //javascript function to pickup the object in this room
-        var pickup = function (roomIndex) {
-            var itemIndex;
-            if (objectLocations[roomIndex] > 0 && objectLocations[roomIndex] < 100) {
-                itemIndex = objectLocations[roomIndex];
-                inventory[inventory.length] = itemIndex;
-                objectLocations[roomIndex] = 999;
-                alert(objectLocations[roomIndex]);
-            }
-        }
-        //This function  loops through the object location array and returns
-        function getObjectForRoom(currentRoom) {
-            var roomIndex = -1;
-            for (var i = 0; i < objectLocations.length ; i++) {
-                if (objectLocations[i] == currentRoom)
-                    roomIndex = i;
-            }
-            return roomIndex
-        }
-        //This is a method/function that shows the game screen. If we look in deatil at this function we can see that 
-        //it uses another function DisplayText to show each line of the screen.
-        function DisplayGameScreen() {
-            //clear the output div
-            $display.empty();
-            //Display the screen output text - note this does not include the buttons
-            DisplayText("You are now in the :");
-            DisplayText(rooms[currentRoom]);
-            DisplayText("Exits: " + ShowAdjacentRooms(exits[currentRoom]) + "<br />");
-            DisplayText('DB:' + currentRoom + 'Light:' + lightLevel + "Hits:" + hits);
-            if (getObjectForRoom(currentRoom) != -1) {
-                var index = getObjectForRoom(currentRoom);
-                DisplayText("You can see " + GameObjects[index]);
-            }
-            //If there is something in our inventory then display it
-            if (inventory.length > 0) {
-                DisplayText("You are carrying: ");
-                for (var i = 0; i < inventory.length ; i++) {
-                    DisplayText("-" + GameObjects[inventory[i]]);
-                }
-            }
-            if (message != null)
-                DisplayText(message.toUpperCase());
-            //Game over code
-            if (IsDragonAlive) {
-                $('#GameOverDiv').hide();
-                $('#GameDiv').show();
-            }
-            else {
-                $('#GameOverDiv').show();
-                $('#GameDiv').hide();
-            }
-            message = "What?";
-        }
-        //Replaces the indexOf js function as i have found it doesn't always work for me!!!!!!!!
-        function checkIndex(issFullArray, issToCheck) {
-            for (i = 0; i < issFullArray.length; i++) {
-                if (issFullArray[i] == issToCheck) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        //Uses the text for a room to build a string that shows which rooms are next to the current room
-        function ShowAdjacentRooms(e) {
-            var newExits = "";
-            if (e != null) {
-                for (i = 0; i < e.length; i++) {
-                    if (i === e.length - 1) {
-                        newExits += e.substring(i, i + 1);
-                    } else if (i === e.length - 2) {
-                        newExits += e.substring(i, i + 1) + " & ";
-                    } else {
-                        newExits += e.substring(i, i + 1) + ", ";
-                    }
-                }
-            }
-            return newExits;
-        }
-        //Simple js function to display a line of text
-        function DisplayText(text) {
-            $display.html($display.html().toString() + text + "<br>");
-        }
-        //Each round we call this function to do all the main game processing 
-        function ProcessGameRound(command) {
-            //Remove any spaces from the command text
-            trimCommand = $.trim(command);
-            //Process command takes the players action
-            ProcessCommand(command);
-            //NOw that we have taken the players logic we need to activate the main game room logic
-            if (currentRoom == 10 && OgreAlive) {
-                //if you are fighting the ogre and you have the spells
-                if (checkIndex(inventory, 3)) {
-                    message += "\<br\>YOU attack the ogre with magic spells and kill him!";
-                    OgreAlive = false;
-                }
-                else {
-                    message += "\<br\>Ogre attacks you!";
-                    hits--;
-                }
-            }
-            //If you are in the final room and the dragon is still alive
-            if (currentRoom == 31 && IsDragonAlive) {
-                //if you are fighting the dragon and you have the oil, burning torch
-                if (checkIndex(inventory, 5) && checkIndex(inventory, 9) && checkIndex(inventory, 12)) {
-                    message += "\<br\>You attack the dragon with oil, burning torch and the wand of Wind - It creates and kill him!";
-                    IsDragonAlive = false; //End Game           
-                }
-                else {
-                    message += "\<br\>The dragon attacks you with firebreath and kills you!";
-                    hits = 0;
-                }
-            }
-            if (currentRoom == 25) {
-                //if you are fighting the gas room burning torch
-                if (checkIndex(inventory, 10)) {
-                    message += "\<br\>The gas in the room is ignited by the torch - You become a human BBQ and die!";
-                    hits = 0;
-                }
-            }
-            DisplayGameScreen();
-        }
-        function ProcessCommand(command) {
-            var direction = command;
-            message = "OK";
-            switch (command) {
-                //Movement Code
-                case "N":
-                    if (exits[currentRoom].indexOf(direction) > -1)
-                        currentRoom -= 8;
-                    else
-                        message = "Can't move there";
-                    break;
-                case "S":
-                    if (exits[currentRoom].indexOf(direction) > -1)
-                        currentRoom += 8;
-                    else
-                        message = "Can't move there";
-                    break;
-                case "E":
-                    if (exits[currentRoom].indexOf(direction) > -1)
-                        currentRoom++;
-                    else
-                        message = "Can't move there";
-                    break;
-                case "W":
-                    if (exits[currentRoom].indexOf(direction) > -1)
-                        currentRoom--;
-                    else
-                        message = "Can't move there";
-                    break;
-                    //End of Movement Code
-                case "P":
-                    pickup(currentRoom);
-                    break
-                case "A":
-                    if (exits[currentRoom].indexOf(direction) > -1)
-                        message = "About ... Game built for #1GAM, LD48 (failed) and my friend Hilary";
-                    break
-                case "?":
-                    message = "The following commands are valid: N S E W P A ?";
-                    break
-            }
-        }
-        //JQuery selector that handles the form submit - 
-        $('#input form').submit(function (evt) {
-            ProcessGameRound($('#userInput').val().toUpperCase());
-            $('#userInput').val('');
-            evt.preventDefault();
-        });
-        //sets the output div to the display variable
-        $display = $('#output');
-        // This is jQuery selector that picks up an event from the button - in this case we look at the value of the button ie. its text and use that 
-        //to call the same function as we would call from the equivalent keyboard command
-        $(".button").click(function (e) {
-            switch (this.value) {
-                case "N":
-                    ProcessGameRound('N');
-                    break;
-                case "S":
-                    ProcessGameRound('S');
-                    break;
-                case "E":
-                    ProcessGameRound('E');
-                    break;
-                case "W":
-                    ProcessGameRound('W');
-                    break;
-                case "F":
-                    ProcessGameRound('F');
-                    break;
-                case "P":
-                    pickup(currentRoom);
-                    break;
-                case "A":
-                    ProcessGameRound('A');
-                    break;
-            }
-        });
-        DisplayGameScreen();
-    });
+// JavaScript Document
 
+var newDerections = document.getElementById("newDerections"); // div of directions for the user
+var introduction= document.getElementById("introduction"); //intro, text that will not be displayed after users firsl answer
+var question1 = document.getElementById("question1");
+var question2 = document.getElementById("question2");
+var question3 = document.getElementById("question3");
+var btn =document.getElementById("btn"); //btn start over
+
+ 
+
+/*event listeners*/
+question1.addEventListener ("submit", getAnswer1); // takes the form (id="question1") and on submit executes the function getAnswer1
+question2.addEventListener ("submit", getAnswer2);
+question3.addEventListener ("submit", getAnswer3);
+
+document.getElementById("btn").addEventListener("click" ,startOver);
+/*functions*/
+function getAnswer1()
+{
+	event.preventDefault(); // to prevent the submition of the form
+	var chosenAnswer1 = document.forms["question1"] ["chosenAnswer1"].value.toLowerCase();
+	console.log (chosenAnswer1);
+	
+	
+	switch (chosenAnswer1)
+	{
+		case "stay home": 
+		newDerections.innerHTML ="<p>Tammy Says: Please Tommy,  I'm bored let's go</p>"+ 
+		"<p><span>Tommy</span> can:<em>stay home</em> or <em>go for adventure</em></p>";
+		introduction.style.display ="none";
+		question1.reset(); // resets the form, so the chosen answer of the user will not appear in the input box
+		break;
+		case "go for adventure":
+		newDerections.innerHTML ="<p>Tommy and Tammy are running to a boat that will take them to the Blueberry Island.</p>"+
+		"<p> On their way, they see a playground. Tammy wants to go there. </p>"+ 
+		"<p> If they stop to play, they will miss the boat.</p>"+
+		"<p> What should they do? <em>go play</em> or <em> run to the boat </em>?</p>";
+		introduction.style.display ="none";
+		question1.style.display ="none";
+		question2.style.display ="block";
+		document.body.classList.add("imgPlayground"); //change the background of the body
+		break;
+		default:
+		newDerections.innerHTML="I don\'t understand, shell we <em>go for adventure</em> or <em>stay home</em>?";
+		introduction.style.display ="none";
+		question1.reset(); 
+		
+	}//end of switch statement
+}  // end of getAnswer1 function
+
+
+function getAnswer2()
+{
+	event.preventDefault();
+	var chosenAnswer2 = document.forms ["question2"] ["chosenAnswer2"].value.toLowerCase();
+	console.log (chosenAnswer2);
+	switch (chosenAnswer2)
+	{
+		case "go play":
+		newDerections.innerHTML = "<p>Tommy and Tammy missed the boat.</p>"+
+		"<p> Now they understand that if you have somewhere important to be, you can't get distracted. " +
+		"They are returning home. Maybe next time they will make the right decision</p>";
+		question2.style.display ="none";
+		btn.style.display ="block";
+		break;
+		
+		case "run to the boat":
+		newDerections.innerHTML = "<p>Tommy and Tammy arrived to the boat on time." +
+			 "They sail to the Blueberry Pie Island.</p>"+
+			"<p> On the island they see a very weird looking boy he is crying.</p>"+
+			"<p>Tammy sayes: Tommy look, this kid is crying, We should help him. </p>"+
+			"<p>Tommy doesn\'t want to help someone that looks so weird. </br> What should they do <em>help</em> or<em> ignore</em>?";
+		question2.style.display ="none";
+		question3.style.display ="block";
+		document.body.classList.add("imgBlueberryIsland");
+		break;
+		default:
+		newDerections.innerHTML="I don\'t understand, shell we <em> run to the boat</em> or <em>go play</em>?";
+		question2.reset();	
+	}//end of switch statement
+	
+} // end of getAnswer2 function
+
+
+function getAnswer3()
+{ 
+	event.preventDefault();
+	var chosenAnswer3 = document.forms ["question3"] ["chosenAnswer3"].value.toLowerCase();
+	console.log (chosenAnswer3);
+	
+	switch (chosenAnswer3)
+	{ 
+		case "help":
+		 newDerections.innerHTML="<p>Tommy and Tammy helped the boy and they become good friends."+
+		 "<p>The boy showed them the best blueberry pie tree and the kids had a lot of fun together.</p>"+
+		 "<p>Tommy and Tammy came home happy. Tomorrow they will find a new adventur.</p>";
+		 btn.style.display ="block"; // the player will be able to paly again
+		 question3.style.display ="none";
+		break;
+		
+		case "ignore":
+		newDerections.innerHTML="<p>Since Tommy and Tammy decided not to help the boy, Tammy felt really bad. "+
+		"She didn\'t want to go around the island and eat delicious fruits from all the blueberry pie trees. "+
+		"The siblings had a horrible experience and came back home unhappy. </p>"+
+		"<p>Tomorrow they might go back to the island and do the right thing, or go to a new adventure.<p>";
+		btn.style.display ="block"; 
+		question3.style.display ="none";
+		break;
+		
+		default:
+		newDerections.innerHTML="I don\'t understand, shell we<em> help</em>  or ignore?";
+		question3.reset();
+	} //end of switch statement
+} // end of getAnswer3 function
+
+
+
+
+function startOver()
+{
+	newDerections.innerHTML ="<p>Tommy and Tammy are at their house thinking of what to do. </p>"+
+         "<p>  - I want to go for adventure! says Tammy.  Let\'s go to the Blueberry Pie Island!</p>"+
+         " <p><span>Tommy</span> can:  <em>stay home</em> or <em>go for adventure</em></p>";
+	question1.reset(); // resets the question1 form, so the chosen answer from before will not appear in the input box
+	question1.style.display ="block";
+	question2.reset();
+	question3.reset();
+	btn.style.display ="none";
+	document.body.classList.remove("imgBlueberryIsland");
+	document.body.classList.remove("imgPlayground"); 
+}
