76ef5b5b770a4cd3716827b23adfa1209c3e3d1e
miko_da_freako <53712054+mikodafreako@users.noreply.github.com>
2019-09-20 16:38:36 -0500

    Update game.js

--- game.js
+++ game.js
@@ -24,39 +24,36 @@ function getAnswer1()
 	{
 		case "boy": 
 		newDerections.innerHTML ="<p>(Oh, looks like he's a boy.)</p>"+ 
-		"<p>The boy was Asian, and had a generic asian face. He had black hair and looked like he was deep in thought.  <br>  </p>";+
-		"<p>He was wearing a black dress shirt, which complimented his black dress pants and dress shoes. He looked really chill and mature.  <br>  </p>";+
-		"<p>(Woah. Pretty emo, but he reminds of myself. I could probably be friends with this guy.) <br>  </p>";+
-		"<p>He was just standing there on the blue grass, and so I decided to ask for his name. <br>  </p>"; +
-		"<p>I honestly didn't care about asking about what this place was or how to get home, <br>  </p>";+
-		"<p>Because this seemed like a place where absolutely nothing could cause me stress. <br>  </p>";+
-		"<p>\"Hey, would you mind telling me your name?\", I ask. <br>  </p>";+	
-		"<p>He didn't give any response. <br>  </p>";+	
-		"<p>\"Heyyy, are you alive?\", I ask. <br>  </p>";+
-		"<p>I'm pretty sure he didn't even notice I was there. <br>  </p>";+
-		"<p>(Guess he doesn't want to talk. I'll just look at the stars with him then.) <br>  </p>";
-		"<p>I stand quietly by his side and start to gaze at the scenery with him. <br>  </p>";
+		"<p>The boy was Asian, and had a generic asian face. He had black hair and looked like he was deep in thought.  <br>  </p>"+
+		"<p>He was wearing a black dress shirt, which complimented his black dress pants and dress shoes. He looked really chill and mature.  <br>  </p>"+
+		"<p>(Woah. Pretty emo, but he reminds of myself. I could probably be friends with this guy.) <br>  </p>"+
+		"<p>He was just standing there on the blue grass, and so I decided to ask for his name. <br>  </p>" +
+		"<p>I honestly didn't care about asking about what this place was or how to get home, <br>  </p>"+
+		"<p>Because this seemed like a place where absolutely nothing could cause me stress. <br>  </p>"+
+		"<p>\"Hey, would you mind telling me your name?\", I ask. <br>  </p>"+	
+		"<p>He didn't give any response. <br>  </p>"+	
+		"<p>\"Heyyy, are you alive?\", I ask. <br>  </p>"+
+		"<p>I'm pretty sure he didn't even notice I was there. <br>  </p>"+
+		"<p>(Guess he doesn't want to talk. I'll just look at the stars with him then.) <br>  </p>"+
+		"<p>I stand quietly by his side and start to gaze at the scenery with him. <br>  </p>"
 		introduction.style.display ="none";
 		question1.reset(); // resets the form, so the chosen answer of the user will not appear in the input box
 		break;
 			
 		case "girl":
 		newDerections.innerHTML ="<p>(Oh, looks like she's a girl.)</p>"+
-		"<p> On their way, they see a playground. Tammy wants to go there. </p>"+ 
-		"<p> If they stop to play, they will miss the boat.</p>"+
-		"<p> What should they do? <em>go play</em> or <em> run to the boat </em>?</p>";
-		"<p>The girl was Asian, and her face was kind of alluring to me. Her long black hair was flowing along with the wind.<br>  </p>";+
-		"<p>She was wearing a white dress, which complimented her white heels. She looked really elegant and civilized. <br>  </p>";+
-		"<p>(She looks really pretty, but I'm not sure how to approach her.) <br>  </p>";+
-		"<p>She was just sitting there cross-legged on the blue grass, and so I decided to ask for her name. <br>  </p>"; +
-		"<p>I honestly didn't care about asking about what this place was or how to get home, <br>  </p>";+
-		"<p>Because this seemed like a place where absolutely nothing could cause me stress. <br>  </p>";+
-		"<p>\"Hey, would you mind telling me your name?\", I ask. <br>  </p>";+	
-		"<p>She didn't give any response. <br>  </p>";+	
-		"<p>\"Heyyy, are you alive?\", I ask. <br>  </p>";+
-		"<p>I'm pretty sure she didn't even notice I was there. <br>  </p>";+
-		"<p>(Guess she doesn't want to talk. I'll just look at the stars with her then.) <br>  </p>";+
-		"<p>I quietly sit cross-legged next to her we start to gaze at the scenery together.<br>  </p>";
+		"<p>The girl was Asian, and her face was kind of alluring to me. Her long black hair was flowing along with the wind.<br>  </p>"+
+		"<p>She was wearing a white dress, which complimented her white heels. She looked really elegant and civilized. <br>  </p>"+
+		"<p>(She looks really pretty, but I'm not sure how to approach her.) <br>  </p>"+
+		"<p>She was just sitting there cross-legged on the blue grass, and so I decided to ask for her name. <br>  </p>" +
+		"<p>I honestly didn't care about asking about what this place was or how to get home, <br>  </p>"+
+		"<p>Because this seemed like a place where absolutely nothing could cause me stress. <br>  </p>"+
+		"<p>\"Hey, would you mind telling me your name?\", I ask. <br>  </p>"+	
+		"<p>She didn't give any response. <br>  </p>"+	
+		"<p>\"Heyyy, are you alive?\", I ask. <br>  </p>"+
+		"<p>I'm pretty sure she didn't even notice I was there. <br>  </p>"+
+		"<p>(Guess she doesn't want to talk. I'll just look at the stars with her then.) <br>  </p>"+
+		"<p>I quietly sit cross-legged next to her we start to gaze at the scenery together.<br>  </p>"
 		introduction.style.display ="none";
 		question1.style.display ="none";
 		question2.style.display ="block";
@@ -70,6 +67,7 @@ function getAnswer1()
 		
 	}//end of switch statement
 }  // end of getAnswer1 function
+
 
 
 function getAnswer2()
